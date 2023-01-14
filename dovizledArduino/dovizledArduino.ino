95b3293fd1d4f807b0af344556c7a0fbb1ad35cb
24FeiFei1 <103454109+24FeiFei1@users.noreply.github.com>
2023-01-14 14:12:34 +0300

    proje dosyası

---
+++ dovizledArduino/dovizledArduino.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // serial haberleşmeyi 9600baud kuruyoruz
+  Serial.begin(9600);
+ 
+  // pin 9,10u output olarak tanımla
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+ 
+void loop() {
+  // gelen veri var mı kontrol ediyoruz
+  if (Serial.available() > 0) {
+    // gelen veriyi okutuyoruz
+    String veri = Serial.readString();
+ 
+    // eğer serialden gelen yazı green ise pin 9 u yüksek pin10u düşük yap
+    if (veri == "green") {
+      digitalWrite(9, HIGH);
+      digitalWrite(10, LOW);
+    }
+    // eğer serialden gelen yazı red ise pin 10 u yüksek pin9u düşük yap
+    else if (veri == "red") {
+      digitalWrite(10, HIGH);
+      digitalWrite(9, LOW);
+    }
+  }
+}
