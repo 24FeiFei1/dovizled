6cabfe8ea5621c3e3c2b67944b22dc12e284f4ec
24FeiFei1 <103454109+24FeiFei1@users.noreply.github.com>
2023-06-07 21:34:05 +0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -14,12 +14,12 @@ void loop() {
     String veri = Serial.readString();
  
     // eğer serialden gelen yazı green ise pin 9 u yüksek pin10u düşük yap
-    if (veri == "Green") {
+    if (veri == "G") {
       digitalWrite(9, HIGH);
       digitalWrite(10, LOW);
     }
     // eğer serialden gelen yazı red ise pin 10 u yüksek pin9u düşük yap
-    else if (veri == "Red") {
+    else if (veri == "R") {
       digitalWrite(10, HIGH);
       digitalWrite(9, LOW);
     }
